38dc3815649bb2e83f8187e90d3daaa8726ce92f
KathatepMingkwan <kathatep@hotmail.com>
2026-05-12 16:15:05 +0700

    update 1.7

---
+++ script.js
@@ -0,0 +1,50 @@
+var OLMap = null;
+var StaticFeatures = new ol.Collection();
+var PlaneIconFeatures = new ol.Collection();
+var PlaneTrailFeatures = new ol.Collection();
+var Planes = {};
+var PlanesOrdered = [];
+var SelectedPlane = null;
+var FollowSelected = false;
+
+function processReceiverUpdate(data) {
+    var now = data.now;
+    var acs = data.aircraft;
+
+
+}
+
+#โหลดข้อมูล
+function fetchData() {
+
+}
+
+function initialize() {
+
+}
+
+function start_load_history() {
+
+}
+
+function load_history_item(i) {
+
+}
+
+function end_load_history() {
+    $("#loader").addClass("hidden");
+
+    console.log("Done loading history");
+
+    if (PositionHistoryBuffer.length > 0) {
+        var now, last=0;
+    }
+}
+
+function make_geodesic_circle(center, radius, points) {
+    var lat1 = center[1] * Math.PI / 180.0;
+    var geom = new ol.geom.LineString();
+    for (var i = 0; i <= points; ++i) {
+        var bearing = 
+    }
+}
