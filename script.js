e6ae2efe093bfbba2c51507265667d5cf0abe817
KathatepMingkwan <kathatep@hotmail.com>
2026-05-12 16:23:31 +0700

    update 1.8

--- script.js
+++ script.js
@@ -45,6 +45,35 @@ function make_geodesic_circle(center, radius, points) {
     var lat1 = center[1] * Math.PI / 180.0;
     var geom = new ol.geom.LineString();
     for (var i = 0; i <= points; ++i) {
-        var bearing = 
+        var bearing = i * 2 *Math.PI / points;
+
+        var lat2 = Math.asin()
     }
+}
+
+function initialize_map() {
+
+}
+
+function reaper() {
+    var newPlanes = [];
+    for (var i = 0; i < PlanesOrdered.length; ++i) {
+        var plane = PlanesOrdered[i];
+    }
+}
+
+function refreshPageTitle() {
+
+}
+
+function refreshSelected() {
+
+}
+
+function refreshTableInfo() {
+
+}
+
+function compareAplha() {
+
 }
